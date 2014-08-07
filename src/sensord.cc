87f6e47ec1e02ef11d02e60925311314182da24f
Christian Kellner <christian@kellner.me>
2014-08-07 19:55:45 +0200

    [sensord] convert to daemon

--- src/sensord.cc
+++ src/sensord.cc
@@ -23,17 +23,16 @@
 #include <wire.h>
 #include <iic.h>
 #include <util.h>
-
+#include <daemon.h>
 
 static void
-mcp_loop(zmq::context_t &zmq_ctx, i2c::mcp9808 &mcp)
+mcp_loop(unix::daemon::ctx &ctx, i2c::mcp9808 &mcp)
 {
+    zmq::context_t &zmq_ctx = ctx.zmq();
     zmq::socket_t publisher(zmq_ctx, ZMQ_PUB);
     publisher.connect("inproc://sensors");
 
-    zmq::socket_t hangman(zmq_ctx, ZMQ_SUB);
-    hangman.connect("inproc://hangman");
-    hangman.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+    zmq::socket_t hangman = ctx.sradio().subscribe();
 
     zmq_pollitem_t items[] = {
         {static_cast<void *>(hangman), 0, ZMQ_POLLIN, 0 }
@@ -64,14 +63,14 @@ mcp_loop(zmq::context_t &zmq_ctx, i2c::mcp9808 &mcp)
 }
 
 static void
-htu_loop(zmq::context_t &zmq_ctx, i2c::htu21d &sensor)
+htu_loop(unix::daemon::ctx &ctx, i2c::htu21d &sensor)
 {
+    zmq::context_t &zmq_ctx = ctx.zmq();
+
     zmq::socket_t publisher(zmq_ctx, ZMQ_PUB);
     publisher.connect("inproc://sensors");
 
-    zmq::socket_t hangman(zmq_ctx, ZMQ_SUB);
-    hangman.connect("inproc://hangman");
-    hangman.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+    zmq::socket_t hangman = ctx.sradio().subscribe();
 
     zmq_pollitem_t items[] = {
             {static_cast<void *>(hangman), 0, ZMQ_POLLIN, 0 }
@@ -101,35 +100,17 @@ htu_loop(zmq::context_t &zmq_ctx, i2c::htu21d &sensor)
     }
 }
 
-
-// signal handling
-static std::atomic<bool> run_the_loop;
-
-static void stop_signal_handler (int signal_value)
-{
-    std::cout << "Got the stop signal. Stopping." << std::endl;
-    run_the_loop = false;
-}
-
-static void init_signals()
-{
-    struct sigaction sig_action;
-
-    sig_action.sa_handler = stop_signal_handler;
-    sig_action.sa_flags = 0;
-    sigemptyset (&sig_action.sa_mask);
-
-    sigaction (SIGINT, &sig_action, NULL);
-    sigaction (SIGTERM, &sig_action, NULL);
-}
-
 int main(int argc, char **argv)
 {
-    printf("Temperature from MCP9808, Humidity from HTU21D\n");
+    unix::daemon daemon{argv[0]};
+    unix::daemon::ctx &ctx = daemon.daemonize();
+
+    daemon.info("noisebox sensor daemon running");
+
     i2c::mcp9808 mcp_dev = i2c::mcp9808::open(1, 0x18);
     i2c::htu21d  htu_dev = i2c::htu21d::open(1, 0x40);
 
-    zmq::context_t context(1);
+    zmq::context_t &context = ctx.zmq();
 
     zmq::socket_t hangman(context, ZMQ_PUB);
     hangman.bind("inproc://hangman");
@@ -140,30 +121,31 @@ int main(int argc, char **argv)
     zmq::socket_t emitter(context, ZMQ_XPUB);
     emitter.bind("tcp://*:5556");
 
-    run_the_loop = true;
-    init_signals();
-
-    std::thread mcp_thread(mcp_loop, std::ref(context), std::ref(mcp_dev));
-    std::thread htu_thread(htu_loop, std::ref(context), std::ref(htu_dev));
-    //std::thread dst_thread(datastore_loop, std::ref(context));
+    std::thread mcp_thread(mcp_loop, std::ref(ctx), std::ref(mcp_dev));
+    std::thread htu_thread(htu_loop, std::ref(ctx), std::ref(htu_dev));
 
     //now the main runloop
     zmq_pollitem_t items[] = {
+            {static_cast<void *>(hangman), 0, ZMQ_POLLIN, 0 },
             {static_cast<void *>(collector), 0, ZMQ_POLLIN, 0 },
             {static_cast<void *>(emitter), 0, ZMQ_POLLIN, 0 }
     };
 
 
-    while (run_the_loop) {
+    while (true) {
         zmq::message_t msg;
 
         util::poll(items);
 
         if (items[0].revents & ZMQ_POLLIN) {
-            util::forward_msg(msg, collector, emitter);
+            break;
         }
 
         if (items[1].revents & ZMQ_POLLIN) {
+            util::forward_msg(msg, collector, emitter);
+        }
+
+        if (items[2].revents & ZMQ_POLLIN) {
             util::forward_msg(msg, emitter, collector);
         }
     }
@@ -173,7 +155,6 @@ int main(int argc, char **argv)
 
     mcp_thread.join();
     htu_thread.join();
-    //dst_thread.join();
 
     return 0;
 }
